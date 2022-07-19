9702dab7cf15a15dbb4ca20cba21386de869a72f
tareqferdous <16101017@uap-bd.edu>
2022-07-19 11:29:01 +0600

    port changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
